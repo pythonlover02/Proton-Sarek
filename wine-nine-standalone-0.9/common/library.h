35913d4e238fbee0fd9cea156c7f47fe41559eda
pythonlover02 <pythonloverv2@gmail.com>
2024-10-17 20:02:45 -0300

    Delete wine-nine-standalone-0.9 directory

--- wine-nine-standalone-0.9/common/library.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: LGPL-2.1-or-later */
-
-#ifndef __COMMON_LIBRARY_H
-#define __COMMON_LIBRARY_H
-
-void *common_load_d3dadapter(char **path, char **err);
-
-#endif /* __COMMON_LIBRARY_H */
